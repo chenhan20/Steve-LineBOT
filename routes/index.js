d7ec141625f558b036f176691b6ddf76e61d6297
SteveChuang <a0911558945@gmail.com>
2019-03-07 00:08:20 +0800

    commit 不知道為啥還是連不上

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var linebot = require('linebot');
+const linebot = require('linebot');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
-router.get('/linewebhook', );
 
 var bot = linebot({
   channelId: '1553484708',
   channelSecret: '6325bb545b2c56c5e4f293d1415635dc',
   channelAccessToken: 'N16uq6aydWNjlGByU31ETxxui6Qk/JUkl7Wd3sdWHgKP5uF4xNYJtRATe7g1I+4EL7oRQvY01hKnwNfgVDKuOjVGDN3Stp2NatpbT+c9zdcNUVZJZSlnujkb8u1Yv65RCFyRwUtF+9xtZb9aMosiqgdB04t89/1O/w1cDnyilFU='
 });
+
+const linebotParser = bot.parser();
+router.post('/linewebhook', linebotParser);
+
 
 // 當有人傳送訊息給Bot時
 bot.on('message', function (event) {
