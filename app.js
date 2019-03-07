93f6c6b95a65b142440b55d82b97b067d2601b38
SteveChuang <steve.chuang@careline.com.tw>
2019-03-07 17:41:12 +0800

    commit

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ var linebot = require('linebot');
 
 // 用於辨識Line Channel的資訊
 var bot = linebot({
-  channelId: '替換成你的CHANNEL_ID',
-  channelSecret: '替換成你的CHANNEL_SECRET',
-  channelAccessToken: '替換成你的CHANNEL_ACCESS_TOKEN'
+  channelId: '',
+  channelSecret: '',
+  channelAccessToken: ''
 });
 
 // 當有人傳送訊息給Bot時
 bot.on('message', function (event) {
   // event.message.text是使用者傳給bot的訊息
   // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
-  event.reply(event.message.text).then(function (data) {
+  event.reply('你說:' + event.message.text).then(function (data) {
     // 當訊息成功回傳後的處理
   }).catch(function (error) {
     // 當訊息回傳失敗後的處理
